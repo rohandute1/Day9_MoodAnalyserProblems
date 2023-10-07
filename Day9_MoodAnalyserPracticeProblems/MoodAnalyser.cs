b4029106af8cb1dc2f6d11e97b955cc785d8d44e
Rohan <rohandute1@gmail.com>
2023-10-07 14:05:51 +0530

    [rohan] Successfully added functionality that check wheather mood is happy or sad

---
+++ Day9_MoodAnalyserPracticeProblems/MoodAnalyser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_MoodAnalyserPracticeProblems
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            // Convert the message to lowercase for case-insensitive comparison
+            string lowercaseMessage = message.ToLower();
+
+            if (lowercaseMessage.Contains("happy"))
+            {
+                return "Happy";
+            }
+            else if (lowercaseMessage.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Cannot determine mood";
+            }
+        }
+    }
+}
--- Day9_MoodAnalyserPracticeProblems/Program.cs
+++ Day9_MoodAnalyserPracticeProblems/Program.cs
@@ -11,7 +11,13 @@ namespace Day9_MoodAnalyserPracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyser problems");
+            Console.WriteLine("Enter a message: ");
+            string message = Console.ReadLine();
 
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string mood = moodAnalyser.AnalyseMood();
+
+            Console.WriteLine($"Mood: {mood}");
             Console.ReadLine();
         }
     }
