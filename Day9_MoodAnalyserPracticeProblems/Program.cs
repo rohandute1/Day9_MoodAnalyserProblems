2cb93e8e5e93f666d5e85264427f0fd61d0c8f34
Rohan <rohandute1@gmail.com>
2023-10-07 13:11:15 +0530

    [rohan] Successfully added initial welcome message

---
+++ Day9_MoodAnalyserPracticeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_MoodAnalyserPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser problems");
+
+            Console.ReadLine();
+        }
+    }
+}
